9f5c74b8e1ecdb92e6e739c7369ce87f65f2b26f
psychohight <7298@holbertonstudents.com>
2024-10-23 13:09:20 +0200

    7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -3,19 +3,19 @@
 /**
  * main - Entry point of the program
  *
- * This function prints the numbers from 0 to 9
+ * This function prints the aplhabet from 0 to 9
  * followed by a newline.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i = 48;
+	int l = 122;
 
-	while (i <= 57)
+	while (l <= 97)
 	{
 	putchar(i);
-	i++;
+	l++;
 	}
 
 	putchar('\n');
