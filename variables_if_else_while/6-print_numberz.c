974ffa3fd1a260f48e2b14e20fa4763ed9387725
psychohight <7298@holbertonstudents.com>
2024-10-23 12:55:47 +0200

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the numbers from 0 to 9
+ * followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i = 48;
+
 	while (i <= 57)
 	{
 	putchar(i);
 	i++;
 	}
+
 	putchar('\n');
 	return (0);
 }
