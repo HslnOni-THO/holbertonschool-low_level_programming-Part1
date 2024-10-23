6fef2c23c6198a10988fc5cb34db885b37fc7ed8
psychohight <7298@holbertonstudents.com>
2024-10-23 12:36:03 +0200

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
 
-/**
- * main - Affiche les chiffres de 0 à 9
- *
- * Return: Toujours 0 (Succès)
- */
 int main(void)
 {
-    int num;
-
-    for (num = 0; num <= 9; num++)
-    {
-        write(1, &num, 1);
-    }
-    write(1, "\n", 1);
-
-    return (0);
+	int i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
 }
