a4503cd4c90fc12fc4d26812fc9c8459cb6926f6
psychohight <7298@holbertonstudents.com>
2024-10-23 12:25:59 +0200

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -11,7 +11,8 @@ int main(void)
 
     for (num = 0; num <= 9; num++)
     {
-        write(1, &num, 1);
+        int digit = num + '0'; // Convertir le chiffre en caractère ASCII
+        write(1, &digit, 1);
     }
     write(1, "\n", 1);
 
