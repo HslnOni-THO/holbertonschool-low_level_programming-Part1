5a7e6c5ef0d874b884a230e028c8e8be6414289b
psychohight <7298@holbertonstudents.com>
2024-10-24 11:47:36 +0200

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <main.h>
+#include "main.h"
 /**
  * main - Prints the alphabet in uppercase
  *
@@ -9,4 +7,15 @@
  * Return: Always 0 (Success)
  */
 void print_alphabet(void);
+{
+char letter = 'a';
 
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+
+return (0);
+}
