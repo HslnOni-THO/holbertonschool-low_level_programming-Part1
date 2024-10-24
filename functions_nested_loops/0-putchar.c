ec55aca13a7c0815a79d7e06d03a8f4222dfe16b
psychohight <7298@holbertonstudents.com>
2024-10-24 11:12:27 +0200

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
  * Description: PRINTS "_putchar" followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 _putchar('_');
@@ -17,8 +17,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-
 _putchar('\n');
-
 return (0);
 }
