cdfd7290e467188753cfc5a70b9a9c6e574bb581
psychohight <7298@holbertonstudents.com>
2024-10-24 11:53:24 +0200

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,16 +6,16 @@
  * followed by a new line. Only _putchar can be used.
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-char (letter = 'a');
+char letter = 'a';
 
-while (letter <= 'z');
+while (letter <= 'z')
 {
 _putchar(letter);
 letter++;
 }
 _putchar('\n');
 
-return (0);
+return;
 }
