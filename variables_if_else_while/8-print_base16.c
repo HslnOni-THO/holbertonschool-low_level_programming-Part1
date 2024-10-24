7d03081b7207a58878da1e3e9abc470589d97ee6
psychohight <7298@holbertonstudents.com>
2024-10-24 12:39:19 +0200

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int n;
+        for (n = 0; n < 10; n++)
+                putchar(n + '0');
+        for (n = 0; n < 6; n++)
+                putchar(n + 'a');
+        putchar('\n');
+
