cdce69108f7f420afef81f89f2f0287ed177c394
psychohight <7298@holbertonstudents.com>
2024-10-24 15:40:35 +0200

    6-main.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
+ *
+ * Return: 1 and prints + if n is greater than zero
+ *         0 and prints 0 if n is zero
+ *         -1 and prints - if n is less than zero
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
