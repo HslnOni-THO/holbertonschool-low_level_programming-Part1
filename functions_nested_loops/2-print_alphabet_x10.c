cd84b66f328ee4e0a342b05a57e1c7262596e127
psychohight <7298@holbertonstudents.com>
2024-10-24 13:37:42 +0200

    2-print_the_aphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints the alphabet 10 times
+ * print_alphabet_x10 - Entry point of the program
  *
- * Description: PRINTS "print_alphabet" followed by a new line
- * followed by a new line. Only _putchar can be used.
+ * Description: Prints a quote using the puts function
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	for (i = 0; i < 10; i++)
+const char *alphabet = "abcdefghijklmnopqrstuvwxyz\n";
+int num;
+for (num = 0; num < 10; num++)
 {
-	print_alphabet();
-	}
+const char *current = alphabet;
+while (*current)
+{_putchar(*current++);
+}
+}
 }
