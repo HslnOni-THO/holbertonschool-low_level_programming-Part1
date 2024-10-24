a149da55b0a68b7f1bdeb744cc77bc9045df90c9
psychohight <7298@holbertonstudents.com>
2024-10-24 11:05:42 +0200

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,21 +3,22 @@
 /**
  * main - Entry point of the program
  *
+ * Description: PRINTS "_putchar" followed by a new line
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 
-_putchar("\n");
+_putchar('\n');
 
 return (0);
 }
