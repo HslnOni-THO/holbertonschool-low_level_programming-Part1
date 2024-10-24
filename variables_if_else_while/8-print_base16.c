1b638a7b41eed932bc7ac5d73469867fcb206dec
psychohight <7298@holbertonstudents.com>
2024-10-24 12:40:50 +0200

    8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,10 +7,11 @@
  */
 int main(void)
 {
-        int n;
-        for (n = 0; n < 10; n++)
-                putchar(n + '0');
-        for (n = 0; n < 6; n++)
-                putchar(n + 'a');
-        putchar('\n');
-
+int n;
+for (n = 0; n < 10; n++)
+putchar(n + '0');
+for (n = 0; n < 6; n++)
+putchar(n + 'a');
+putchar('\n');
+return (0);
+}
