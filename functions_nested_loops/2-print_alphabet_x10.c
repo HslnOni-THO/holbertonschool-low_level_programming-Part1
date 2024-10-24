826db536ad742cffe7f66ea0d2bdb27c618bd086
psychohight <7298@holbertonstudents.com>
2024-10-24 12:54:42 +0200

    2-print_the_aphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet - Prints the alphabet in uppercase
+ * print_alphabet- Prints the alphabet in uppercase
  *
  * Description: PRINTS "print_alphabet" followed by a new line
  * followed by a new line. Only _putchar can be used.
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void);
 {
 char letter = 'a';
 
