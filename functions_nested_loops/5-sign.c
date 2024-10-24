c56127aaf39e3184156d8d9d8570ca0162cd1635
psychohight <7298@holbertonstudents.com>
2024-10-24 15:34:50 +0200

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
- * _sign - prints the sign of a number
- * @n: The character to check
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
  *
- * Description: Prints a quote using the puts function
- * Returns 1 and prints + if n is greater than zero
- * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
+ * Return: 1 and prints + if n is greater than zero
+ *         0 and prints 0 if n is zero
+ *         -1 and prints - if n is less than zero
  */
-
 int print_sign(int n)
 {
 	if (n < 0)
